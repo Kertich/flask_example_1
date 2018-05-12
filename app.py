2eff5e4fff0df0a812c8dd1aedb8fb495bb60da2
Kertich <kipsangkertich2gmail.com>
2018-05-12 15:43:12 +0300

    Add app file

--- app.py
+++ app.py
@@ -1,2 +1,14 @@
 from flask import Flask
 
+app = Flask(__name__)
+
+@app.route("/")
+def homepage():
+    return "This is our homepage."
+
+@app.route("/login")
+def login():
+    return "This is our login."
+
+if __name__ == "__main__":
+    app.run(debug=True)
